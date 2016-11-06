0ed731fb34ba43f1775fba51d3ef6232b159c802
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-11-06 14:17:35 +0900

    Added packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="wraptools",
+    version="1.0",
+    author="Hiroki Kiyohara",
+    author_email="hirokiky@gmail.com",
+    description="Utility tools for wrapping and dispatching functions",
+)
