0ebb1b36494e1c198a0dc802a40f66ec6d1f7c63
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-11-06 13:25:45 +0900

    Added django's request.method dispatcher

--- wraptools/contrib/django/dispatchers.py
+++ wraptools/contrib/django/dispatchers.py
@@ -2,3 +2,17 @@ def is_authenticated(request, *args, **kwargs):
     """ Dispatcher for django views functions to dispatch to authentication views
     """
     return request.user.is_authenticated
+
+
+def method(method_name):
+    """ Dispatcher to route when request.method is same with :param method_name:
+    """
+    def dispatcher(request, *args, **kwargs):
+        return request.method == method_name
+    return dispatcher
+
+
+method_get = method("GET")
+method_post = method("POST")
+method_put = method("PUT")
+method_delete = method("DELETE")
