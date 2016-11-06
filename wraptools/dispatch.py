138b4c1438012243b122d3ac5524c238b18dccc0
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-11-06 13:42:48 +0900

    Added utilities dispatcher functions

--- wraptools/dispatch.py
+++ wraptools/dispatch.py
@@ -28,3 +28,60 @@ def dispatch(*dispatchers, default=None):
             return default(*args, **kwargs)
 
     return wrapped
+
+
+def all_(*dispatchers):
+    """ Taking :arg dispatchers: functions and routing when all of dispatchers returns True
+    """
+    def dispatcher(*args, **kwargs):
+        return all(d(*args, **kwargs) for d in dispatchers)
+    return dispatcher
+
+
+def any_(*dispatchers):
+    """ Taking :arg dispatchers: and routing when some dispatchers returns True
+    """
+    def dispatcher(*args, **kwargs):
+        return any(d(*args, **kwargs) for d in dispatchers)
+    return dispatcher
+
+
+def not_(dispatcher):
+    """ Taking :arg dispatcher: and routing when the dispatcher returns False
+    """
+    def _dispatcher(*args, **kwargs):
+        return not dispatcher(*args, **kwargs)
+    return _dispatcher
+
+
+class dispatcher:
+    """ Decorator applying utilities for wrapped function.
+    Basically dispatcher is just a function which returns bool values, but when
+    decorating this, dispatcher functions can be combined and inverted.
+
+    >>> @dispatcher
+    ... def even(n):
+    ...     return n % 2 == 0
+    ...
+    >>> @dispatcher
+    ... def multiple_three(n):
+    ...     return n % 3 == 0
+    ...
+    >>> even & multiple_three  # New dispatcher that routes when multiple of 6.
+    >>> even | multiple_three  # New dispatcher that routes when multiple of 2 or 3.
+    >>> ~even  # New dispatcher that routes when odd
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def __and__(self, other):
+        return self.__class__(all_(self.func, other.func))
+
+    def __or__(self, other):
+        return self.__class__(any_(self.func, other.func))
+
+    def __invert__(self, other):
+        return self.__class__(not_(self.func))
