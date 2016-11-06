a1c5404c622d26cfe9e7933637b8a6fadaa96ca8
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-11-06 18:27:31 +0900

    Improved docs and packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
 
 
 setup(
     name="wraptools",
     version="1.0",
+    packages=find_packages(),
     author="Hiroki Kiyohara",
     author_email="hirokiky@gmail.com",
     description="Utilities for wrapping and dispatching functions",
+    long_description=README,
 )
